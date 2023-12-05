65b01bca56529ddd9454ea74377bf95ecdda23d9
buonoalex <loopatthecity@gmail.com>
2023-12-05 14:30:53 +0100

    Start project

---
+++ src/org/lessons/java/geometria/Main.java
@@ -0,0 +1,7 @@
+package org.lessons.java.geometria;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/org/lessons/java/geometria/Rettangolo.java
@@ -0,0 +1,19 @@
+package org.lessons.java.geometria;
+
+public class Rettangolo {
+    private int altezza;
+    private int base;
+
+    //Costruttore Personale
+    public Rettangolo(int altezza,int base){
+        this.altezza = altezza;
+        this.base = base;
+    }
+
+    //Costruttore Default
+    public Rettangolo(){
+
+    }
+
+}
+
