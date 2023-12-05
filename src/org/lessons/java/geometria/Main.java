85631572e1bb98d4bb3479c9b5b8520dd99c3cbf
buonoalex <loopatthecity@gmail.com>
2023-12-05 14:35:24 +0100

    Add Scanner and Create Rettangolo

--- src/org/lessons/java/geometria/Main.java
+++ src/org/lessons/java/geometria/Main.java
@@ -1,7 +1,32 @@
 package org.lessons.java.geometria;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        //Apro Scanner
+        Scanner myBroScan = new Scanner(System.in);
+
+        //Variabili
+        int altezza;
+        int base;
+
+        //Input
+        System.out.println("Inserisci la dimensione della base del rettangolo : ");
+        base = Integer.parseInt(myBroScan.nextLine());
+
+        System.out.println("Inserisci la dimensione dell'altezza del rettangolo : ");
+        altezza = Integer.parseInt(myBroScan.nextLine());
+
+        //Accesso a oggetto e Creazione
+        Rettangolo rett1 = new Rettangolo(altezza,base);
+
+
+
+
+        //Chiudo Scanner
+        myBroScan.close();
 
     }
 }
